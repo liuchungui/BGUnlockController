a30169a415bfa27a7cb8813fe2ee6b91aeaf1b5c
chunguiLiu <404468494@qq.com>
2015-11-27 17:42:24 +0800

    add

--- BGUnlockControllerDemo/AppDelegate.m
+++ BGUnlockControllerDemo/AppDelegate.m
@@ -42,4 +42,8 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application willContinueUserActivityWithType:(NSString *)userActivityType {
+    return YES;
+}
+
 @end
